bbde82332ed91341d51f2145f431751095e5c6df
Manuel Lopez <jmlopez.rod@gmail.com>
2023-10-01 11:00:45 -0500

    create simple action

---
+++ src/square_number.py
@@ -0,0 +1,13 @@
+import os
+
+def append_to_output(var_name: str, var_value: str) -> None:
+    with open(os.environ.get("GITHUB_OUTPUT"), 'a') as f:
+        f.write(f'{var_name}={var_value}\n')
+
+def main() -> None:
+    num = os.environ.get('INPUT_NUM')
+    result = num * num
+    append_to_output('num-square', str(result))
+
+if __name__ == '__main__':
+    main()
